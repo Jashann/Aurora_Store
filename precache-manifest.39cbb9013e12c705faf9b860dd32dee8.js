b7be7bc145e5b72ad261c0595ca9582a85694358
Jashann <48613250+Jashann@users.noreply.github.com>
2020-09-14 14:33:06 +0530

    Updates

---
+++ precache-manifest.39cbb9013e12c705faf9b860dd32dee8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a36ec5c66637a13e1c44da003ef0f1db",
+    "url": "/Aurora_Store/index.html"
+  },
+  {
+    "revision": "451c4a3bc564229aa8c0",
+    "url": "/Aurora_Store/static/css/main.831695f3.chunk.css"
+  },
+  {
+    "revision": "9a4f5bb87674c57b7808",
+    "url": "/Aurora_Store/static/js/2.1d0a60fe.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/Aurora_Store/static/js/2.1d0a60fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "451c4a3bc564229aa8c0",
+    "url": "/Aurora_Store/static/js/main.68872956.chunk.js"
+  },
+  {
+    "revision": "f99b3571e86b489ed861",
+    "url": "/Aurora_Store/static/js/runtime-main.eacd3ea2.js"
+  },
+  {
+    "revision": "434a2f5358f598c80634975049f22c3f",
+    "url": "/Aurora_Store/static/media/logo.434a2f53.png"
+  }
+]);
